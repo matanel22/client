c58c2c369400c1168f5974d7f01127490d19eab4
matanel22 <49matanel@gmail.com>
2022-11-15 14:09:57 +0200

    aaaaaaaa

---
+++ build/precache-manifest.dc79bf7a4ebf4ab73697cf86cfd02113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8436b982fc01753f966f",
+    "url": "/static/css/main.71b6b9c2.chunk.css"
+  },
+  {
+    "revision": "8436b982fc01753f966f",
+    "url": "/static/js/main.8436b982.chunk.js"
+  },
+  {
+    "revision": "7df547cdee2bef1d3dfc",
+    "url": "/static/js/1.7df547cd.chunk.js"
+  },
+  {
+    "revision": "d5dbcf0a59f300ede6fb",
+    "url": "/static/js/2.d5dbcf0a.chunk.js"
+  },
+  {
+    "revision": "481725b9cb569ba070f1",
+    "url": "/static/js/runtime~main.481725b9.js"
+  },
+  {
+    "revision": "4e7cbcebaa92dd3182e4e8e2fe411e69",
+    "url": "/index.html"
+  }
+];
--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -57,6 +57,7 @@ export default function Home() {
     if (isPass.includes("@")) {
       setValidEmail(true);
       // setValidPass(false)
+
       return;
     }
     histury.push("/projects");
